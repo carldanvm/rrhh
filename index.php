e7227375297775b0ca1e089b5f87781557d97b17
Carlos Vieira <dv60682@gmail.com>
2024-10-14 12:08:02 -0400

    index preparado para manejar rutas

--- index.php
+++ index.php
@@ -3,8 +3,17 @@
     if (isset($_GET["page"])) {
         $page = $_GET["page"];
 
-        echo "<h1>$page</h1>";   
+        switch ($page) {
+            case 'ejemplo':
+                include 'ejemplo.php';
+                break;
+            
+            default:
+                echo "<h1>Error 404</h1>";
+                break;
+        }
+
     }else{
-        echo "<h1>404</h1>";
+        echo "<h1>Error 404</h1>";
     }
 ?>

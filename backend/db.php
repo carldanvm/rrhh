84f63daf429e0728480fab867e5350e7aee21bd0
Carlos Vieira <dv60682@gmail.com>
2024-10-17 19:17:57 -0400

    pagina para probar base de datos, y arreglado error en index.php

---
+++ backend/db.php
@@ -0,0 +1,10 @@
+<?php 
+    /* Conexion a la base de datos */
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $db = "rrhh";
+
+    $conn = mysqli_connect($host, $user, $password, $db);
+
+?>

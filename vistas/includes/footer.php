14a6dad2491a28d9bdbf465057faa1f57c595ccb
Carlos Vieira <dv60682@gmail.com>
2024-11-02 11:54:52 -0400

    bootstrap y jquery instalado

--- vistas/includes/footer.php
+++ vistas/includes/footer.php
@@ -1,2 +1,4 @@
+    <script src="js/bootstrap.js"></script>
+    <script src="js/jquery-3.7.1.min.js"></script>
 </body>
 </html>
--- vistas/includes/header.php
+++ vistas/includes/header.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css/bootstrap.css" rel="stylesheet">
     <title>RRHH</title>
+
 
     <style>
         h1 {

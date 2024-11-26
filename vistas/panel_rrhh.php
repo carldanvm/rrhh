7797782425692c7b6e5f7fc3ae43c18ed31dad69
Carlos Vieira <dv60682@gmail.com>
2024-11-25 21:53:06 -0400

    cambios estilo panel rrhh

--- vistas/panel_rrhh.php
+++ vistas/panel_rrhh.php
@@ -1,8 +1,24 @@
 <?php include "includes/header.php"; ?>
 
-<div class="row" style="margin-top: 20px; min-height: 100vh">
+<style>
+    /* Ocultar el encabezado solo en esta vista */
+    .encabezado h1{
+        display: none;
+    }
+    .encabezado{
+        background-color: white;
+        box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.2);
+        position: relative;
+    }
+    .contenedor{
+        box-shadow: none;
+    }
+</style>
+
+<div class="row" style="margin: 0; min-height: 100%; min-width: 100%;">
     <!-- Barra lateral de opciones -->
     <div class="col-2 barra-lateral">
+        <div class="barra-lateral-logo">Logo</div>
         <div class="barra-lateral-boton">
             <a href="index.php?page=registro_empleados" >Registro empleados</a>
         </div>
